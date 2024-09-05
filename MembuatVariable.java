24031bb675d45431b8c95f6fb50eac23a991664a
Daryl <2382029@unai.edu>
2024-09-05 11:12:57 +0700

    Handson 1

---
+++ MembuatVariable.java
@@ -0,0 +1,26 @@
+public class MembuatVariable {
+    public static void main(String[] args) {
+        // Variable bagaikan sebuah
+        // Tipe data bagaikan sebuah label
+
+        // Deklarasi
+        int tipeDataInteger;
+
+        // Inisialisasi
+        tipeDataInteger = 20;
+
+        // Deklarasi dan Inisialisasi
+        long tipeDataLong = 30L;
+
+        // bagaimana penamaan variable yang baik
+        // pastikan nama variable adalah kata benda
+
+        long showInfo; // kata kerja itu untuk method
+
+        // camelCase -> DarylGrandyKarundeng
+        // kebab-case
+        // snake_case
+
+        final String FULL_NAME = "DARYL KARUNDENG";
+    }
+}
---
+++ TipeDataString.java
@@ -0,0 +1,17 @@
+public class TipeDataString {
+    public static void main(String[] args) {
+        // Non primitive
+        String firstName = "Daryl";
+        String lastName = "Karundeng";
+
+        // Concatenation
+        String fullName1 = firstName + " " + lastName;
+        String fullName2 = firstName.concat(str:" ").concat(str:lastName);
+
+
+        // Primitive
+        int  tipeDataInteger = 10;
+        tipeDataInteger = null;
+
+    }
+}
